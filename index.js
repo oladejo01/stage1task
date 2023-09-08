f3bfac2f7ecca591130171262d6eb9afaf04f09b
unknown <dtnoladejo@gmail.com>
2023-09-08 22:00:03 +0100

    second commit

--- index.js
+++ index.js
@@ -4,50 +4,51 @@ const time = document.getElementById("time");
 
     const date = new Date();
     const dayName = date.getDay();
+    
     const hour = date.getUTCHours();
     const minute = date.getUTCMinutes();
+    const second = date.getUTCSeconds();
+
 
     console.log(dayName);
     console.log(hour);
     console.log(minute);
 
-    time.value = `${hour + 1} : ${minute}`;
+    time.innerHTML = `${hour + 1} : ${minute} : ${second}`;
 
     switch (dayName) {
         case 0 :
-        Day.value = "Sunday";
+        Day.innerHTML = "Sunday";
             break;
 
      case 1:
-        Day.value = "Monday";
+        Day.innerHTML = "Monday";
             break;
             
      case 2: 
-        Day.value = "Tuesday";
+        Day.innerHTML = "Tuesday";
             break;
             
      case 3: 
-        Day.value= "Wednesday";
+        Day.innerHTML= "Wednesday";
             break;     
 
      case 4 :
-        Day.value = "Thursday"
+        Day.innerHTML = "Thursday"
         break;  
 
      case 5 :
-        Day.value = "Friday"
+        Day.innerHTML = "Friday"
            break;  
 
      case 6 :
-        Day.value = "Thursday"
+        Day.innerHTML = "Thursday"
               break;
 
         default:
-        Day.value = "date not available"
+        Day.innerHTML = "date not available"
             break;
     } 
 
-
- console.log(Day.value);
 
 
